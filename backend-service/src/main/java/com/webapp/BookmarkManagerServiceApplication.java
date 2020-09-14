54ac9c847b829fcdd1ba904396bb0da49a3ec902
unknown <manasvijangid21@gmail.com>
2020-09-15 03:30:46 +0530

    Initial commit

---
+++ backend-service/src/main/java/com/webapp/BookmarkManagerServiceApplication.java
@@ -0,0 +1,27 @@
+package com.webapp;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
+
+@SpringBootApplication
+public class BookmarkManagerServiceApplication {
+
+    private static Logger logger = LoggerFactory.getLogger(BookmarkManagerServiceApplication.class);
+    public static void main(String[] args) {
+
+        SpringApplication.run(BookmarkManagerServiceApplication.class, args);
+
+        logger.info("************************************************************");
+        logger.info("************BOOKMARK MANAGER STARTED SUCCESSFULLY***********");
+        logger.info("************************************************************");
+
+    }
+
+}
+
